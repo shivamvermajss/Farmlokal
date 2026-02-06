0dc353fdfc7cdb9f9b3790c0738f35f277842d64
Shivam Verma <shivamvermacse2026@gmail.com>
2026-02-06 15:39:58 +0530

    chore: add db connection logging

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,13 @@
 const mysql = require('mysql2/promise')
 
+console.log('Attempting to connect to database with config:', {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 3306,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  // password: '***' // Do not log password
+})
+
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT || 3306,
@@ -10,7 +18,8 @@ const pool = mysql.createPool({
     rejectUnauthorized: false
   },
   waitForConnections: true,
-  connectionLimit: 10
+  connectionLimit: 10,
+  connectTimeout: 10000 // 10s timeout
 })
 
 module.exports = pool
